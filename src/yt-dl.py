cc790ae006ac448b1160ae42d0999638a29d5e8e
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-02-18 19:22:52 +0000

    tests

--- src/yt-dl.py
+++ src/yt-dl.py
@@ -1,14 +1,35 @@
 from __future__ import unicode_literals
 import youtube_dl
+import sys
 
 if __name__ == '__main__':
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download(['http://www.youtube.com/watch?v=BaW_jenozKc'])
+
+    # Example command - yt-dl.py [video_url] [type] [final_path]
+
+    if len(sys.argv) == 4:
+
+        ydl_opts = {}
+
+        # Determin the URL to download.
+        video_url = sys.argv[1]
+
+        if sys.argv[2] == 'audio':
+            # We want the audio only.
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+            }
+        elif sys.argv[2] == 'video':
+            ydl_opts = {}
+
+        print(ydl_opts)
+        print(video_url)
+
+        #with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        #    ydl.download(['http://www.youtube.com/watch?v=BaW_jenozKc'])
+    else:
+        print('Incorrect args. ' + str(len(sys.argv)))
